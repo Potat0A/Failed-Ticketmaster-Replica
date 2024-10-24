8a496d20c7e81837c146fab527b3008fbaaa4ba4
Null <44486209+Potat0A@users.noreply.github.com>
2024-10-24 15:29:25 +0800

    yes

--- App/__init__.py
+++ App/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 
 app = Flask(__name__)
 
@@ -6,8 +6,14 @@ app = Flask(__name__)
 def home():
     return "Main Page"
 
+@app.route("</name>")
 def user(name):
     return f"Hello{name}"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
 
 if __name__=="__main__":
     app.run()
